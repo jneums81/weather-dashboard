a92ce1cf9d43535887a4192db4f198da6464eca8
Jackson Neuman <jacksonneuman@Jacksons-Air-4.attlocal.net>
2023-10-02 20:04:36 -0500

    added css file and updated html and js

--- script.js
+++ script.js
@@ -1,260 +1,142 @@
-// Add an event listener for form submission
-searchForm.addEventListener('submit', function (event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
+// Define your API key and base URL
+const apiKey = '0e6c7f3a3e6cc010b40e84fa4133a8ca';
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
-    // Get the user's input from the cityInput element
-    const userInput = cityInput.value;
+// Variables to store references to HTML elements
+const searchForm = document.getElementById('searchForm');
+const cityInput = document.getElementById('cityInput');
+const currentWeatherContainer = document.getElementById('currentWeatherContainer');
+const forecastContainer = document.getElementById('forecastContainer');
+const searchHistoryContainer = document.getElementById('searchHistoryContainer');
 
-    // Call a function to fetch weather data with the user's input
-    fetchWeatherData(userInput);
+// Event listener for form submission
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const userCity = cityInput.value;
+    fetchWeatherData(userCity);
 });
 
-// Define the fetchWeatherData function to fetch weather data based on the user's input
+// Fetch weather data from OpenWeather API
 function fetchWeatherData(city) {
-    // Construct the API URL using the user's input and API key
-    const apiUrl = `${baseUrl}weather?q=${city}&appid=${apiKey}`;
+    // Construct the API URL
+    const apiUrl = `${baseUrl}/weather?q=${city}&appid=${apiKey}`;
 
-    // Fetch data from the API using the constructed URL
+    // Fetch data from the API
     fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                // Handle the case where the API request returns an error
-                throw new Error('City not found or other API error');
-            }
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            // Check if the API response contains an error message
+            // Check if the city is found
             if (data.cod === '404') {
-                // Handle the case where the city is not found
-                alert('City not found. Please enter a valid city name.');
-                return;
+                alert('City not found.');
+            } else {
+                displayCurrentWeather(data);
+                // Extract latitude and longitude from current weather data and call the forecast function
+                const { coord } = data;
+                fetchForecast(coord.lat, coord.lon);
+                updateSearchHistory(city);
             }
-
-            // Process the data and update the currentWeatherContainer
-            // with the current weather information
-            updateCurrentWeatherUI(data);
-
-            // You can also store the search history and update the searchHistoryContainer
-            // with the latest search
-            updateSearchHistory(city);
         })
-        .catch(error => {
-            console.error('Error fetching weather data:', error);
-            // Handle other errors that may occur during the fetch
-        });
-        // Inside the fetchWeatherData function after successfully fetching data
-        displayCurrentWeather(data);
-
-    // Process the data and update the currentWeatherContainer
-    // with the current weather information
-    updateCurrentWeatherUI(data);
-
-    // Extract lat and lon from the current weather API response
-    const lat = data.coord.lat;
-    const lon = data.coord.lon;
-
-    // Call a function to fetch the 5-day forecast using lat and lon
-    fetchFiveDayForecast(lat, lon);
-
-    // You can also store the search history and update the searchHistoryContainer
-    // with the latest search
-    updateSearchHistory(city);
+        .catch(error => console.error('Error fetching data:', error));
 }
 
+// Display current weather conditions
+function displayCurrentWeather(data) {
+    // Extract relevant data
+    const { name, dt, weather, main, wind } = data;
+    const iconUrl = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
+    const date = new Date(dt * 1000).toLocaleDateString();
+    const temp = main.temp;
+    const humidity = main.humidity;
+    const windSpeed = wind.speed;
 
-
-
-
-// Define a function to update the UI with current weather data
-function updateCurrentWeatherUI(weatherData) {
-    // Update the currentWeatherContainer with the weather information
-    // You can create and append HTML elements to display the data
-    // For example:
-    currentWeatherContainer.innerHTML = `
-        <h2>Current Weather in ${weatherData.name}</h2>
-        <p>Temperature: ${weatherData.main.temp}°C</p>
-        <p>Weather Condition: ${weatherData.weather[0].description}</p>
-        <!-- Add more data as needed -->
+    // Create HTML elements to display data
+    const currentWeatherHtml = `
+        <h2>${name} (${date})</h2>
+        <img src="${iconUrl}" alt="${weather[0].description}">
+        <p>Temperature: ${temp} &#8451;</p>
+        <p>Humidity: ${humidity}%</p>
+        <p>Wind Speed: ${windSpeed} m/s</p>
     `;
+
+    // Update currentWeatherContainer
+    currentWeatherContainer.innerHTML = currentWeatherHtml;
 }
 
-// Define a function to update the search history
+// Fetch and display 5-day forecast
+function fetchForecast(lat, lon) {
+    // Construct the API URL for forecast
+    const forecastApiUrl = `${baseUrl}/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+    // Fetch forecast data from the API
+    fetch(forecastApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayForecast(data.list);
+        })
+        .catch(error => console.error('Error fetching forecast data:', error));
+}
+
+// Display 5-day forecast
+function displayForecast(forecastData) {
+    // Loop through the forecast data and create HTML elements for each day
+    const forecastHtml = forecastData.slice(0, 5).map(forecast => {
+        const { dt, weather, main, wind } = forecast;
+        const iconUrl = `http://openweathermap.org/img/w/${weather[0].icon}.png`;
+        const date = new Date(dt * 1000).toLocaleDateString();
+        const temp = main.temp;
+        const humidity = main.humidity;
+        const windSpeed = wind.speed;
+
+        return `
+            <div class="forecast-day">
+                <h3>${date}</h3>
+                <img src="${iconUrl}" alt="${weather[0].description}">
+                <p>Temperature: ${temp} &#8451;</p>
+                <p>Humidity: ${humidity}%</p>
+                <p>Wind Speed: ${windSpeed} m/s</p>
+            </div>
+        `;
+    }).join('');
+
+    // Update forecastContainer
+    forecastContainer.innerHTML = forecastHtml;
+}
+
+// Update search history
 function updateSearchHistory(city) {
-    // Get the existing search history from local storage or initialize it as an empty array
+    // Get the existing search history from local storage or initialize an empty array
     let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-    // Add the searched city to the search history array
+    // Add the searched city to the history
     searchHistory.push(city);
 
-    // Store the updated search history array in local storage
+    // Store the updated history in local storage
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 
-    // Call a function to update the search history list in the UI
-    displaySearchHistory(searchHistory);
+    // Display the updated history
+    displaySearchHistory();
 }
 
-
-// Define a function to display the current weather conditions
-function displayCurrentWeather(weatherData) {
-    // Extract relevant data from the API response
-    const cityName = weatherData.name;
-    const date = new Date(weatherData.dt * 1000); // Convert timestamp to date
-    const iconUrl = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    const temperature = weatherData.main.temp;
-    const humidity = weatherData.main.humidity;
-    const windSpeed = weatherData.wind.speed;
-
-    // Create HTML elements to display the data
-    const weatherInfoContainer = document.createElement('div');
-    weatherInfoContainer.classList.add('weather-info');
-
-    const cityElement = document.createElement('h2');
-    cityElement.textContent = `Current Weather in ${cityName}`;
-
-    const dateElement = document.createElement('p');
-    dateElement.textContent = `Date: ${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-
-    const iconElement = document.createElement('img');
-    iconElement.src = iconUrl;
-    iconElement.alt = weatherData.weather[0].description;
-
-    const temperatureElement = document.createElement('p');
-    temperatureElement.textContent = `Temperature: ${temperature}°C`;
-
-    const humidityElement = document.createElement('p');
-    humidityElement.textContent = `Humidity: ${humidity}%`;
-
-    const windSpeedElement = document.createElement('p');
-    windSpeedElement.textContent = `Wind Speed: ${windSpeed} m/s`;
-
-    // Append the created elements to the weatherInfoContainer
-    weatherInfoContainer.appendChild(cityElement);
-    weatherInfoContainer.appendChild(dateElement);
-    weatherInfoContainer.appendChild(iconElement);
-    weatherInfoContainer.appendChild(temperatureElement);
-    weatherInfoContainer.appendChild(humidityElement);
-    weatherInfoContainer.appendChild(windSpeedElement);
-
-    // Update the currentWeatherContainer with the weatherInfoContainer
-    currentWeatherContainer.innerHTML = ''; // Clear previous content
-    currentWeatherContainer.appendChild(weatherInfoContainer);
-}
-
-// Define a function to fetch the 5-day forecast using lat and lon
-function fetchFiveDayForecast(lat, lon) {
-    // Construct the API URL for the 5-day forecast using lat, lon, and the API key
-    const forecastUrl = `${baseUrl}forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-    // Fetch data from the 5-day forecast API
-    fetch(forecastUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error fetching 5-day forecast data');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Process the 5-day forecast data and display it
-            displayForecast(data);
-        })
-        .catch(error => {
-            console.error('Error fetching 5-day forecast data:', error);
-            // Handle errors
-        });
-}
-
-// Define a function to display the 5-day forecast
-function displayForecast(forecastData) {
-    // Clear the forecastContainer
-    forecastContainer.innerHTML = '';
-
-    // Loop through the forecast data
-    forecastData.list.forEach(forecast => {
-        // Extract relevant data for each forecast
-        const date = new Date(forecast.dt * 1000);
-        const iconUrl = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-        const temperature = forecast.main.temp;
-        const humidity = forecast.main.humidity;
-        const windSpeed = forecast.wind.speed;
-
-        // Create HTML elements to display the forecast data
-        const forecastItem = document.createElement('div');
-        forecastItem.classList.add('forecast-item');
-
-        const dateElement = document.createElement('p');
-        dateElement.textContent = `Date: ${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-
-        const iconElement = document.createElement('img');
-        iconElement.src = iconUrl;
-        iconElement.alt = forecast.weather[0].description;
-
-        const temperatureElement = document.createElement('p');
-        temperatureElement.textContent = `Temperature: ${temperature}°C`;
-
-        const humidityElement = document.createElement('p');
-        humidityElement.textContent = `Humidity: ${humidity}%`;
-
-        const windSpeedElement = document.createElement('p');
-        windSpeedElement.textContent = `Wind Speed: ${windSpeed} m/s`;
-
-        // Append the created elements to the forecastItem
-        forecastItem.appendChild(dateElement);
-        forecastItem.appendChild(iconElement);
-        forecastItem.appendChild(temperatureElement);
-        forecastItem.appendChild(humidityElement);
-        forecastItem.appendChild(windSpeedElement);
-
-        // Append the forecastItem to the forecastContainer
-        forecastContainer.appendChild(forecastItem);
-    });
-}
-
-// Define a function to display the search history in the UI
-function displaySearchHistory(searchHistory) {
-    // Clear the search history list
-    searchHistoryList.innerHTML = '';
-
-    // Loop through the search history array and create list items for each search
-    searchHistory.forEach(city => {
-        const listItem = document.createElement('li');
-        listItem.textContent = city;
-
-        // Add an event listener to each list item to allow re-searching
-        listItem.addEventListener('click', () => {
-            // Call the fetchWeatherData function with the clicked city
-            fetchWeatherData(city);
-        });
-
-        // Append the list item to the search history list
-        searchHistoryList.appendChild(listItem);
-    });
-}
-
-// Add a click event listener to the searchHistoryContainer
-searchHistoryContainer.addEventListener('click', function (event) {
-    if (event.target.tagName === 'LI') {
-        // Check if the clicked element is an <li> (list item)
-        const selectedCity = event.target.textContent;
-
-        // Call the fetchWeatherData function with the selected city
-        fetchWeatherData(selectedCity);
-    }
-});
-
-// Function to load and display search history during initial page load
-function loadSearchHistory() {
+// Display search history
+function displaySearchHistory() {
+    // Get the search history from local storage
     const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-    
-    // Call the displaySearchHistory function to display the search history
-    displaySearchHistory(searchHistory);
+
+    // Create a clickable list of cities
+    const historyHtml = searchHistory.map(city => {
+        return `<li class="history-item">${city}</li>`;
+    }).join('');
+
+    // Add event listeners to history items
+    searchHistoryContainer.innerHTML = `<ul>${historyHtml}</ul>`;
+    const historyItems = searchHistoryContainer.querySelectorAll('.history-item');
+    historyItems.forEach(item => {
+        item.addEventListener('click', () => {
+            const selectedCity = item.textContent;
+            fetchWeatherData(selectedCity);
+        });
+    });
 }
 
-// Call the loadSearchHistory function when the page loads
-window.addEventListener('load', loadSearchHistory);
-
-
-
-
-
+// Initial page load: Load and display search history
+displaySearchHistory();
